d255421e70ece36e580b4b50cde39286410c5d9d
longer <longer_lung@aseglobal.com>
2021-10-20 14:11:35 +0800

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Image_label_tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderTextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.next = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.clean_btn = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1054, 34);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "讀入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Location = new System.Drawing.Point(323, 36);
+            this.folderTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(703, 29);
+            this.folderTextBox.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(57, 96);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 706);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(1176, 96);
+            this.next.Margin = new System.Windows.Forms.Padding(4);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(112, 34);
+            this.next.TabIndex = 3;
+            this.next.Text = "Next";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(321, 96);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(705, 705);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(126, 872);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(843, 28);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 14F);
+            this.label1.Location = new System.Drawing.Point(18, 872);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 28);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "進度條";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("PMingLiU", 14F);
+            this.label2.Location = new System.Drawing.Point(1077, 872);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 28);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "/";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1054, 96);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Prev";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.prev_Click);
+            // 
+            // clean_btn
+            // 
+            this.clean_btn.Location = new System.Drawing.Point(1210, 856);
+            this.clean_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.clean_btn.Name = "clean_btn";
+            this.clean_btn.Size = new System.Drawing.Size(92, 44);
+            this.clean_btn.TabIndex = 12;
+            this.clean_btn.Text = "清除";
+            this.clean_btn.UseVisualStyleBackColor = true;
+            this.clean_btn.Click += new System.EventHandler(this.clean_btn_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1257, 39);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(57, 12);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1183, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "輔助框";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 915);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.clean_btn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.folderTextBox);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "iLabel v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox folderTextBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button clean_btn;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Image_label_tool
+{
+
+    public partial class Form1 : Form
+    {
+        // 資料夾路徑
+        string folderPath;
+        int pmax = 0, pnow = 0; //檔案進度
+        string[] files;
+        bool cl = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            //當前路徑
+            //string current_dir = System.Environment.CurrentDirectory;
+
+            StreamReader sr = new StreamReader(@"config.label.txt");
+            while (!sr.EndOfStream)
+            {
+                // 每次讀取一行，直到檔尾
+                string line = sr.ReadLine();
+                AddNewButton(line);
+            }
+            sr.Close();
+            Shown += (o, e) => MessageBox.Show("longer_lung@aseglobal.com","聯絡資訊");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            //fdb.RootFolder = Environment.SpecialFolder.MyDocuments;
+            folderDialog.Description = "選擇存放要標記的圖片的資料夾...";
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // 清除listbox1
+                listBox1.Items.Clear();
+
+                folderPath = folderDialog.SelectedPath;
+                folderTextBox.Text = folderPath;
+
+                files = Directory.GetFiles(folderPath);
+                string[] dirs = Directory.GetDirectories(folderPath);
+                if (files.Length >= 0)
+                {
+                    progressBar1.Value = 0;
+                    progressBar1.Maximum = files.Length - 1;
+                    pmax = files.Length;
+                    label2.Text = String.Format("{0}/{1}", pnow + 1, pmax);
+                    foreach (string file in files)
+                    {
+                        listBox1.Items.Add(Path.GetFileName(file));
+                    }
+                    // listbox focus on first
+                    listBox1.SelectedIndex = 0;
+                    listBox1.Focus();
+
+                    if (pictureBox1.Image != null) pictureBox1.Image.Dispose();
+                    if (cl)
+                    {
+                        pictureBox1.Image = Add_WarningLine_Into_Img(Image.FromFile(files[pnow]));
+                    }
+                    else {
+                        pictureBox1.Image = Image.FromFile(files[pnow]);
+                    }
+                    
+                    
+                }
+            }
+#if DEBUG
+                MessageBox.Show(folderDialog.SelectedPath);
+#endif
+
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            //NextImgNoLabel();
+            Next();
+        }
+
+        private void prev_Click(object sender, EventArgs e)
+        {
+            //PrevImgNoLabel();
+            Prev();
+        }
+
+
+        int c = 0;
+        public System.Windows.Forms.Button AddNewButton(string line)
+        {
+            System.Windows.Forms.Button btn = new System.Windows.Forms.Button();
+            this.Controls.Add(btn);
+            btn.Top = c * 28 + 100;
+            btn.Left = 764;
+            btn.Text = line;
+            btn.Click += new System.EventHandler(btn_Click);
+            c = c + 1;
+            return btn;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string label = (sender as Button).Text;
+
+            NextImg(label);
+        }
+
+
+        void NextImg(string label)
+        {
+            // 檔案名稱
+            SaveToCSV(Path.GetFileName(files[pnow]), label);
+            Next();
+        }
+        void Next()
+        {
+            if (pnow >= pmax - 1)
+            {
+                MessageBox.Show("最後一張照片！");
+            }
+            else
+            {
+                pnow = pnow + 1;
+                progressBar1.Value++;
+                listBox1.SelectedIndex = pnow;
+                listBox1.Focus();
+                if (cl)
+                {
+                    pictureBox1.Image = Add_WarningLine_Into_Img(Image.FromFile(files[pnow]));
+                }
+                else {
+                    pictureBox1.Image = Image.FromFile(files[pnow]);
+                }
+                label2.Text = String.Format("{0}/{1}", pnow + 1, pmax);
+            }
+        }
+        void Prev()
+        {
+            if (pnow <= 0)
+            {
+                MessageBox.Show("第一張照片！");
+            }
+            else
+            {
+                pnow = pnow - 1;
+                progressBar1.Value--;
+                listBox1.SelectedIndex = pnow;
+                listBox1.Focus();
+                if (cl)
+                {
+                    pictureBox1.Image = Add_WarningLine_Into_Img(Image.FromFile(files[pnow]));
+                }
+                else {
+                    pictureBox1.Image = Image.FromFile(files[pnow]);
+                }
+                
+                label2.Text = String.Format("{0}/{1}", pnow + 1, pmax);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string curItem = listBox1.SelectedItem.ToString();
+            int index = listBox1.FindString(curItem);
+            pnow = index;
+            if (cl)
+            {
+                pictureBox1.Image = Add_WarningLine_Into_Img(Image.FromFile(files[pnow]));
+            }
+            else {
+                pictureBox1.Image = Image.FromFile(files[index]);
+            }
+            label2.Text = String.Format("{0}/{1}", index + 1, pmax);
+            progressBar1.Value = index;
+        }
+
+        private void clean_btn_Click(object sender, EventArgs e)
+        {
+            folderTextBox.Text = null;
+
+            progressBar1.Value = 0;
+
+            pnow = 0;
+
+            label2.Text = "/";
+
+            listBox1.Items.Clear();
+
+            pictureBox1.Image.Dispose();
+            pictureBox1.Image = null;
+        }
+
+        void SaveToCSV(string imageName, string label)
+        {
+            /*
+            string[] sArray = imageName.Split('\\');
+            int idx = sArray.Length;
+            imageName = sArray[idx - 1];
+            folderPath
+            */
+#if DEBUG
+            MessageBox.Show(imageName, label);
+#endif
+            string labelFolder = folderPath + @"\Label";
+            if (!Directory.Exists(labelFolder))
+            {
+                Directory.CreateDirectory(labelFolder);
+            }
+
+            //utf-8-BOM 
+            StreamWriter sw = new StreamWriter(labelFolder + @"\label.csv", true, Encoding.UTF8);
+            sw.WriteLine(imageName + "," + label);
+            sw.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                cl = true;
+            }
+            else {
+                cl = false;
+            }
+        }
+
+        private Bitmap Add_WarningLine_Into_Img(Image img)
+        {
+            Bitmap bitmap=null;
+            try
+            {
+                Bitmap image = new Bitmap(new Bitmap(img));
+                Graphics g = Graphics.FromImage(image);
+                Pen pen1 = new Pen(Color.Red, 3);
+                g.DrawLine(pen1, image.Width / 10, image.Height / 10, image.Width - image.Width / 10, image.Height / 10);
+                g.DrawLine(pen1, image.Width - image.Width / 10, image.Height / 10, image.Width - image.Width / 10, image.Height - image.Height / 10);
+                g.DrawLine(pen1, image.Width - image.Width / 10, image.Height - image.Height / 10, image.Width / 10, image.Height - image.Height / 10);
+                g.DrawLine(pen1, image.Width / 10, image.Height - image.Height / 10, image.Width / 10, image.Height / 10);
+                bitmap = image;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return bitmap;
+        }
+    }
+
+}
